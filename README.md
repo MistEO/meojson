344cc97b5cea4816f8dc2454d6c7c733f32c67bf
MistEO <mistereo@hotmail.com>
2020-10-11 02:33:28 +0800

    update usage

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # C++ JSON解析库
 
-**正则是有极限的，我不写正则啦，jojo！**
+**参考`sample/sample.cpp`**
--- sample/sample.cpp
+++ sample/sample.cpp
@@ -6,6 +6,7 @@
 
 int main()
 {
+    /*** Parse: String to Json ***/
     std::ifstream ifs("test.json");
     std::stringstream ibuf;
     ibuf << ifs.rdbuf();
@@ -16,6 +17,23 @@ int main()
     val = json::parser::parse(content);
     std::cout << "parse success" << std::endl;
     std::cout << val.as_object()["configurations"].as_array()[0].as_object()["name"].as_string() << std::endl;
+
+    /*** Ify: Json to String  ***/
+    json::object obj;
+    obj["key1"] = json::value::string("hello");
+    obj["PI"] = json::value::number(3.1416);
+    std::vector<json::value> value_vector;
+    for (int i = 0; i != 10; ++i)
+    {
+        value_vector.push_back(json::value::number(i));
+    }
+    obj["list"] = json::array(value_vector);
+
+    json::object obj2;
+    obj2["obj2_key1"] = json::value::string("i am child");
+    obj["child"] = obj2;
+
+    std::cout << obj.to_string() << std::endl;
 
     return 0;
 }
