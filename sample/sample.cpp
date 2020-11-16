d269c1f160344584daf89b536fd070bced73ef9e
MistEO <mistereo@hotmail.com>
2020-11-16 10:54:46 +0800

    update sample

--- sample/sample.cpp
+++ sample/sample.cpp
@@ -9,46 +9,25 @@ int main()
     {
         std::cout << "****** Parsing ****** " << std::endl;
         std::string content = R"(
-{
-    "version": "0.2.0",
-    "configurations": [
         {
-            "name": "(gdb) 启动",
-            "type": "cppdbg",
-            "request": "launch",
-            "program": "${workspaceFolder}/build/sample.out",
-            "args": [],
-            "stopAtEntry": false,
-            "cwd": "${workspaceFolder}/build",
-            "environment": [],
-            "externalConsole": false,
-            "MIMode": "gdb",
-            "setupCommands": [
-                {
-                    "description": "为 gdb 启用整齐打印",
-                    "text": "-enable-pretty-printing",
-                    "ignoreFailures": true
-                }
-            ],
-            "preLaunchTask": "make_debug",
-            "number_test": 123.46e-31
+            "prject": {
+                "name": "cppjson",
+                "list": [ 
+                    1, 2, 3
+                ]
+            }
         }
-    ]
-}
-)";
+        )";
         //std::cout << content << std::endl;
 
-        auto &&[ret, json] = json::parser::parse(content);
+        auto &&[ret, value] = json::parser::parse(content);
 
         if (ret)
         {
-            std::cout << "parse success" << std::endl;
-
-            std::cout << json["configurations"][0]["name"].as_string() << std::endl;
-            for (auto &&[key, val] : json["configurations"][0]["setupCommands"][0].as_object())
-            {
-                std::cout << key << ": " << val.to_string() << std::endl;
-            }
+            // Output "cppjson"
+            std::cout << value["prject"]["name"].as_string() << std::endl;
+            // Output 2
+            std::cout << value["prject"]["list"][1].as_integer() << std::endl;
         }
         else
         {
@@ -65,19 +44,29 @@ int main()
         json::value json;
         json["lib"] = "cppjson";
         json["Pi"] = 3.1416;
-        json["star"] = 0;
-        json["star"] = json["star"].as_integer() + 1;
+        json["stars"] = 0;
+        json["stars"] = json["stars"].as_integer() + 1;
 
-        json["arr"] = json::array({"a", "b", "c"});
+        json["arr"] = json::array(
+            {"a", "b", "c"}
+        );
 
-        json["obj"] = json::object(
-            {{"key1", "value1"},
-             {"key2", 123}});
+        json["obj_aaa"] = json::object(
+            {
+                {"key1", "value1"},
+                {"key2", 123}
+            }
+        );
 
-        json["obj"]["key3"] = json::array({1, 2, 3});
+        json["obj_bbb"]["key3"] = json::array(
+            {1, 2, 3}
+        );
 
+        // Output "{"obj_bbb":{"key3":[1,2,3]},"obj_aaa":{"key2":123,"key1":"value1"},"arr":["a","b","c"],"Pi":3.141600,"stars":1,"lib":"cppjson"}"
         std::cout << json.to_string() << std::endl;
     }
+
+    std::cout << std::endl;
 
     return 0;
 }
