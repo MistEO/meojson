bd304be8a20abc52b5d57960a7ea9bd71149bd6f
MistEO <mistereo@hotmail.com>
2020-12-04 00:09:28 +0800

    update test script

--- test/parse/conformance_test.sh
+++ test/parse/conformance_test.sh
@@ -5,20 +5,20 @@ ScriptDir=`dirname $ScriptPath`
 ProjectDir=`realpath $ScriptDir/../../`
 BuildDir=$ProjectDir/build
 
-echo -e "\033[32m------------ make meojson conformance test ------------\033[0m"
+echo "\033[32m------------ make meojson conformance test ------------\033[0m"
 make clean -C $ProjectDir
 make debug "SAMPLE_FILE=test/parse/conformance_test.cpp" -C $ProjectDir
 
 NativejsonDir=$BuildDir/nativejson-benchmark
 if [ ! -d $NativejsonDir ];then
-    echo -e "\033[32m------------ git clone nativejson-benchmark ------------\033[0m"
+    echo "\033[32m------------ git clone nativejson-benchmark ------------\033[0m"
     git clone https://github.com/miloyip/nativejson-benchmark.git $NativejsonDir
 # else
-#     echo -e "\033[32m------------ git pull nativejson-benchmark ------------\033[0m"
+#     echo "\033[32m------------ git pull nativejson-benchmark ------------\033[0m"
 #     cd $NativejsonDir
 #     git pull
 #     cd $CurDir
 fi
 
-echo -e "\033[32m------------ testing meojson conformance------------\033[0m"
+echo "\033[32m------------ testing meojson conformance------------\033[0m"
 $BuildDir/conformance_test.out `ls $NativejsonDir/data/*/*`
--- test/parse/performance_test.sh
+++ test/parse/performance_test.sh
@@ -5,33 +5,34 @@ ScriptDir=`dirname $ScriptPath`
 ProjectDir=`realpath $ScriptDir/../../`
 BuildDir=$ProjectDir/build
 
-echo -e "\033[32m------------ make meojson performance test ------------\033[0m"
+echo "\033[32m------------ make meojson performance test ------------\033[0m"
 make clean -C $ProjectDir
 make debug "SAMPLE_FILE=test/parse/performance_test.cpp" -C $ProjectDir
 
 RapidSrcDir=$ScriptDir/rapidjson
 if [ ! -d $RapidSrcDir ];then
-    echo -e "\033[32m------------ git clone rapidjson ------------\033[0m"
+    echo "\033[32m------------ git clone rapidjson ------------\033[0m"
     git clone https://github.com/Tencent/rapidjson.git $RapidSrcDir
 # else
-#     echo -e "\033[32m------------ git pull rapidjson ------------\033[0m"
+#     echo "\033[32m------------ git pull rapidjson ------------\033[0m"
 #     cd $RapidSrcDir
 #     git pull
 #     cd $CurDir
 fi
 
-echo -e "\033[32m------------ make rapidjson performance test ------------\033[0m"
+echo "\033[32m------------ make rapidjson performance test ------------\033[0m"
 RapidBuildDir=$BuildDir/rapidjson_build
 mkdir -p $RapidBuildDir
 cmake $ScriptDir -B $RapidBuildDir
 make -C $RapidBuildDir
 
-echo -e "\033[32m------------ generate a random json file ------------\033[0m"
+echo "\033[32m------------ generate a random json file ------------\033[0m"
 make debug -j5 "SAMPLE_FILE=test/generate/generate_test.cpp" -C $ProjectDir
-$BuildDir/generate_test.out 2 256 > $BuildDir"/rand.json"
+RandJsonPath=$BuildDir/rand.json
+$BuildDir/generate_test.out 2 256 > $RandJsonPath
 
-echo -e "\033[32m\n------------ all ready, start testing ------------\n\033[0m"
-echo -e "\033[32m------------ testing meojson performance------------\033[0m"
-$BuildDir/performance_test.out rand.json 100
-echo -e "\033[32m------------ testing rapidjson performance------------\033[0m"
-$RapidBuildDir/rapidjson_performance_test.out rand.json 100
+echo "\033[32m\n------------ all ready, start testing ------------\n\033[0m"
+echo "\033[32m------------ testing meojson performance------------\033[0m"
+$BuildDir/performance_test.out $RandJsonPath 100
+echo "\033[32m------------ testing rapidjson performance------------\033[0m"
+$RapidBuildDir/rapidjson_performance_test.out $RandJsonPath 100
