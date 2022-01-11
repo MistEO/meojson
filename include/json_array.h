6b3dd4fed50c40583212286833c21deba63034fe
_ChingC <chingc.ccmail@gmail.com>
2022-01-11 12:40:01 +0800

    style: 添加clang-format

--- include/json_array.h
+++ include/json_array.h
@@ -1,97 +1,90 @@
 #pragma once
 
+#include <initializer_list>
 #include <string>
 #include <vector>
-#include <initializer_list>
 
-namespace json
-{
-    class value;
+namespace json {
+class value;
 
-    class array
-    {
-    public:
-        using raw_array = std::vector<value>;
-        using iterator = raw_array::iterator;
-        using const_iterator = raw_array::const_iterator;
-        using reverse_iterator = raw_array::reverse_iterator;
-        using const_reverse_iterator = raw_array::const_reverse_iterator;
+class array {
+ public:
+  using raw_array              = std::vector<value>;
+  using iterator               = raw_array::iterator;
+  using const_iterator         = raw_array::const_iterator;
+  using reverse_iterator       = raw_array::reverse_iterator;
+  using const_reverse_iterator = raw_array::const_reverse_iterator;
 
-        array() = default;
-        array(const array& rhs) = default;
-        array(array&& rhs) noexcept = default;
-        array(const raw_array& arr);
-        array(raw_array&& arr) noexcept;
-        array(std::initializer_list<raw_array::value_type> init_list);
-        template<typename ArrayType>
-        array(ArrayType arr) {
-            static_assert(
-                std::is_constructible<json::value, typename ArrayType::value_type>::value,
-                "Parameter can't be used to construct a json::value");
-            for (auto&& ele : arr) {
-                _array_data.emplace_back(std::move(ele));
-            }
-        }
+  array()                     = default;
+  array(const array& rhs)     = default;
+  array(array&& rhs) noexcept = default;
+  array(const raw_array& arr);
+  array(raw_array&& arr) noexcept;
+  array(std::initializer_list<raw_array::value_type> init_list);
 
-        ~array() noexcept = default;
+  template <typename ArrayType>
+  array(ArrayType arr);
 
-        bool empty() const noexcept { return _array_data.empty(); }
-        size_t size() const noexcept { return _array_data.size(); }
-        bool exist(size_t pos) const { return _array_data.size() < pos; }
-        const value& at(size_t pos) const;
-        const std::string to_string() const;
-        const std::string format(std::string shift_str = "    ", size_t basic_shift_count = 0) const;
+  ~array() noexcept = default;
 
-        const bool get(size_t pos, bool default_value) const;
-        const int get(size_t pos, int default_value) const;
-        const long get(size_t pos, long default_value) const;
-        const unsigned long get(size_t pos, unsigned default_value) const;
-        const long long get(size_t pos, long long default_value) const;
-        const unsigned long long get(size_t pos, unsigned long long default_value) const;
-        const float get(size_t pos, float default_value) const;
-        const double get(size_t pos, double default_value) const;
-        const long double get(size_t pos, long double default_value) const;
-        const std::string get(size_t pos, std::string default_value) const;
-        const std::string get(size_t pos, const char* default_value) const;
+  bool              empty() const noexcept { return _array_data.empty(); }
+  size_t            size() const noexcept { return _array_data.size(); }
+  bool              exist(size_t pos) const { return _array_data.size() < pos; }
+  const value&      at(size_t pos) const;
+  const std::string to_string() const;
+  const std::string format(std::string shift_str         = "    ",
+                           size_t      basic_shift_count = 0) const;
 
-        template <typename... Args>
-        decltype(auto) emplace_back(Args &&... args)
-        {
-            static_assert(
-                std::is_constructible<raw_array::value_type, Args...>::value,
-                "Parameter can't be used to construct a raw_array::value_type");
-            return _array_data.emplace_back(std::forward<Args>(args)...);
-        }
+  const bool               get(size_t pos, bool default_value) const;
+  const int                get(size_t pos, int default_value) const;
+  const long               get(size_t pos, long default_value) const;
+  const unsigned long      get(size_t pos, unsigned default_value) const;
+  const long long          get(size_t pos, long long default_value) const;
+  const unsigned long long get(size_t             pos,
+                               unsigned long long default_value) const;
+  const float              get(size_t pos, float default_value) const;
+  const double             get(size_t pos, double default_value) const;
+  const long double        get(size_t pos, long double default_value) const;
+  const std::string        get(size_t pos, std::string default_value) const;
+  const std::string        get(size_t pos, const char* default_value) const;
 
-        void clear() noexcept;
-        // void earse(size_t pos);
+  template <typename... Args>
+  decltype(auto) emplace_back(Args&&... args) {
+    static_assert(
+        std::is_constructible<raw_array::value_type, Args...>::value,
+        "Parameter can't be used to construct a raw_array::value_type");
+    return _array_data.emplace_back(std::forward<Args>(args)...);
+  }
 
-        iterator begin() noexcept;
-        iterator end() noexcept;
-        const_iterator begin() const noexcept;
-        const_iterator end() const noexcept;
-        const_iterator cbegin() const noexcept;
-        const_iterator cend() const noexcept;
+  void clear() noexcept;
+  // void earse(size_t pos);
 
-        reverse_iterator rbegin() noexcept;
-        reverse_iterator rend() noexcept;
-        const_reverse_iterator rbegin() const noexcept;
-        const_reverse_iterator rend() const noexcept;
-        const_reverse_iterator crbegin() const noexcept;
-        const_reverse_iterator crend() const noexcept;
+  iterator       begin() noexcept;
+  iterator       end() noexcept;
+  const_iterator begin() const noexcept;
+  const_iterator end() const noexcept;
+  const_iterator cbegin() const noexcept;
+  const_iterator cend() const noexcept;
 
-        const value& operator[](size_t pos) const;
-        value& operator[](size_t pos);
+  reverse_iterator       rbegin() noexcept;
+  reverse_iterator       rend() noexcept;
+  const_reverse_iterator rbegin() const noexcept;
+  const_reverse_iterator rend() const noexcept;
+  const_reverse_iterator crbegin() const noexcept;
+  const_reverse_iterator crend() const noexcept;
 
-        array& operator=(const array&) = default;
-        array& operator=(array&&) noexcept = default;
+  const value& operator[](size_t pos) const;
+  value&       operator[](size_t pos);
 
-        // const raw_array &raw_data() const;
+  array& operator=(const array&) = default;
+  array& operator=(array&&) noexcept = default;
 
-    private:
-        raw_array _array_data;
-    };
+  // const raw_array &raw_data() const;
 
-    std::ostream& operator<<(std::ostream& out, const array& arr);
+ private:
+  raw_array _array_data;
+};
 
-} // namespace json
+std::ostream& operator<<(std::ostream& out, const array& arr);
+
+}  // namespace json
