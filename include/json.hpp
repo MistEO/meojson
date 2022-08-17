de0bd69511720c6ed93b4568cfe582f3da8944bf
MistEO <mistereo@hotmail.com>
2022-08-17 23:37:18 +0800

    fix type error

--- include/json.hpp
+++ include/json.hpp
@@ -220,7 +220,9 @@ namespace json
 
         explicit array(const value& val);
         explicit array(value&& val);
-        template<typename ArrayType> array(ArrayType arr);
+        template<typename ArrayType, typename EnableT = std::enable_if_t<
+            std::is_constructible_v<value, typename ArrayType::value_type>>>
+            array(ArrayType arr);
 
         ~array() noexcept = default;
 
@@ -311,7 +313,9 @@ namespace json
         object(std::initializer_list<raw_object::value_type> init_list);
         explicit object(const value& val);
         explicit object(value&& val);
-        template <typename MapType> object(MapType map);
+        template <typename MapType, typename EnableT = std::enable_if_t<
+            std::is_constructible_v<raw_object::value_type, typename MapType::value_type>>>
+            object(MapType map);
 
         ~object() = default;
 
@@ -1052,12 +1056,9 @@ namespace json
         ;
     }
 
-    template<typename ArrayType>
+    template<typename ArrayType, typename EnableT>
     MEOJSON_INLINE array::array(ArrayType arr)
     {
-        static_assert(
-            std::is_constructible<value, typename ArrayType::value_type>::value,
-            "Parameter can't be used to construct a value");
         _array_data.assign(
             std::make_move_iterator(arr.begin()),
             std::make_move_iterator(arr.end()));
@@ -1917,12 +1918,9 @@ namespace json
         return out;
     }
 
-    template <typename MapType> object::object(MapType map)
+    template <typename MapType, typename EnableT>
+    object::object(MapType map)
     {
-        static_assert(std::is_constructible<raw_object::value_type,
-                                            typename MapType::value_type>::value,
-                      "Parameter can't be used to construct a "
-                      "object::raw_object::value_type");
         _object_data.insert(
             std::make_move_iterator(map.begin()),
             std::make_move_iterator(map.end()));
