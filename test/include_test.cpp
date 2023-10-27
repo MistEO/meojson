799c870b71ec1c4c17e6e80314a8330106d8cac3
MistEO <mistereo@hotmail.com>
2023-10-27 10:35:50 +0800

    test: json5

--- test/include_test.cpp
+++ test/include_test.cpp
@@ -1,12 +1,16 @@
 #include "json.hpp"
+#include "json5.hpp"
 
 #include <iostream>
 
 bool include_test()
 {
     json::value j;
-    j["hi"] = "meojson";
+    j["Hi"] = "meojson";
     std::cout << j << std::endl;
+
+    auto opt5 = json::parse5(R"({"Hello":"World"})");
+    std::cout << *opt5 << std::endl;
 
     return true;
 }
