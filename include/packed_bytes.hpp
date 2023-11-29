f0fa087afdc25dcb68d23df48fb5a61252964959
MistEO <mistereo@hotmail.com>
2023-11-29 17:08:46 +0800

    styles: format

--- include/packed_bytes.hpp
+++ include/packed_bytes.hpp
@@ -1,23 +1,27 @@
 #pragma once
+
 #include <cstdint>
 #include <cstring>
 #include <type_traits>
+
 #include "bitops.hpp"
 
 #if defined(__GNUC__) || defined(__clang__)
-#define __packed_bytes_strong_inline __attribute__((always_inline)) 
+#define __packed_bytes_strong_inline __attribute__((always_inline))
 #elif defined(_MSC_VER)
 #define __packed_bytes_strong_inline __forceinline
 #else
 #define __packed_bytes_strong_inline inline
 #endif
 
-struct packed_bytes_trait_none {
+struct packed_bytes_trait_none
+{
     static constexpr bool available = false;
 };
 
 template <size_t N>
-struct packed_bytes {
+struct packed_bytes
+{
     using traits = packed_bytes_trait_none;
 };
 
@@ -27,39 +31,40 @@ struct packed_bytes {
 #include "packed_bytes_arm.hpp"
 #endif
 
-struct packed_bytes_trait_uint64 {
+struct packed_bytes_trait_uint64
+{
     static constexpr bool available = sizeof(void*) >= 8;
     static constexpr auto step = 8;
     using value_type = std::enable_if_t<sizeof(void*) >= 8, uint64_t>;
 
-    __packed_bytes_strong_inline static value_type load_unaligned(const void *ptr) {
+    __packed_bytes_strong_inline static value_type load_unaligned(const void* ptr)
+    {
         value_type result;
         memcpy((void*)&result, ptr, 8);
         return result;
     }
 
-    __packed_bytes_strong_inline static value_type less(value_type x, uint8_t n) {
-        return (((x) - UINT64_C(0x0101010101010101) * (n)) & ~(x) & UINT64_C(0x8080808080808080));
-    }
-
-    __packed_bytes_strong_inline static value_type is_zero_memberwise(value_type v) {
-        return (((v) - UINT64_C(0x0101010101010101)) & ~(v) & UINT64_C(0x8080808080808080));
-    }
-
-    __packed_bytes_strong_inline static bool is_all_zero(value_type v)
+    __packed_bytes_strong_inline static value_type less(value_type x, uint8_t n)
     {
-        return v == UINT64_C(0);
+        return (((x)-UINT64_C(0x0101010101010101) * (n)) & ~(x)&UINT64_C(0x8080808080808080));
     }
 
-    __packed_bytes_strong_inline static value_type equal(value_type x, uint8_t n) {
+    __packed_bytes_strong_inline static value_type is_zero_memberwise(value_type v)
+    {
+        return (((v)-UINT64_C(0x0101010101010101)) & ~(v)&UINT64_C(0x8080808080808080));
+    }
+
+    __packed_bytes_strong_inline static bool is_all_zero(value_type v) { return v == UINT64_C(0); }
+
+    __packed_bytes_strong_inline static value_type equal(value_type x, uint8_t n)
+    {
         return is_zero_memberwise((x) ^ (UINT64_C(0x0101010101010101) * (n)));
     }
 
-    __packed_bytes_strong_inline static value_type bitwise_or(value_type a, value_type b) {
-        return a | b;
-    }
+    __packed_bytes_strong_inline static value_type bitwise_or(value_type a, value_type b) { return a | b; }
 
-    __packed_bytes_strong_inline static size_t first_nonzero_byte(value_type x) {
+    __packed_bytes_strong_inline static size_t first_nonzero_byte(value_type x)
+    {
         if (json::__bitops::is_little_endian())
             return json::__bitops::countr_zero(x) / 8;
         else
@@ -67,38 +72,41 @@ struct packed_bytes_trait_uint64 {
     }
 };
 
-struct packed_bytes_trait_uint32 {
+struct packed_bytes_trait_uint32
+{
     static constexpr bool available = true;
     static constexpr auto step = 4;
     using value_type = uint32_t;
 
-    __packed_bytes_strong_inline static value_type load_unaligned(const void *ptr) {
+    __packed_bytes_strong_inline static value_type load_unaligned(const void* ptr)
+    {
         value_type result;
         memcpy((void*)&result, ptr, 4);
         return result;
     }
 
-    __packed_bytes_strong_inline static value_type less(value_type x, uint8_t n) {
+    __packed_bytes_strong_inline static value_type less(value_type x, uint8_t n)
+    {
         return (((x) - ~UINT32_C(0) / 255 * (n)) & ~(x) & ~UINT32_C(0) / 255 * 128);
     }
 
-    __packed_bytes_strong_inline static value_type is_zero_memberwise(value_type v) {
-        return (((v) - UINT32_C(0x01010101)) & ~(v) & UINT32_C(0x80808080));;
+    __packed_bytes_strong_inline static value_type is_zero_memberwise(value_type v)
+    {
+        return (((v)-UINT32_C(0x01010101)) & ~(v)&UINT32_C(0x80808080));
+        ;
     }
 
-    __packed_bytes_strong_inline static bool is_all_zero(value_type v) {
-        return v == UINT32_C(0);
-    }
+    __packed_bytes_strong_inline static bool is_all_zero(value_type v) { return v == UINT32_C(0); }
 
-    __packed_bytes_strong_inline static value_type equal(value_type x, uint8_t n) {
+    __packed_bytes_strong_inline static value_type equal(value_type x, uint8_t n)
+    {
         return is_zero_memberwise((x) ^ (~UINT32_C(0) / 255 * (n)));
     }
 
-    __packed_bytes_strong_inline static value_type bitwise_or(value_type a, value_type b) {
-        return a | b;
-    }
+    __packed_bytes_strong_inline static value_type bitwise_or(value_type a, value_type b) { return a | b; }
 
-    __packed_bytes_strong_inline static size_t first_nonzero_byte(value_type x) {
+    __packed_bytes_strong_inline static size_t first_nonzero_byte(value_type x)
+    {
         if (json::__bitops::is_little_endian())
             return json::__bitops::countr_zero(x) / 8;
         else
@@ -106,21 +114,22 @@ struct packed_bytes_trait_uint32 {
     }
 };
 template <>
-struct packed_bytes<8> {
+struct packed_bytes<8>
+{
     using traits = std::enable_if_t<packed_bytes_trait_uint64::available, packed_bytes_trait_uint64>;
 };
 
 template <>
-struct packed_bytes<4> {
+struct packed_bytes<4>
+{
     using traits = packed_bytes_trait_uint32;
 };
 
 template <size_t N>
 using packed_bytes_trait = typename packed_bytes<N>::traits;
 
-using packed_bytes_trait_max = std::conditional_t<packed_bytes_trait<32>::available, packed_bytes_trait<32>,
-  std::conditional_t<packed_bytes_trait<16>::available, packed_bytes_trait<16>,
-    std::conditional_t<packed_bytes_trait<8>::available, packed_bytes_trait<8>,
-      packed_bytes_trait<4>
-  >>>;
-
+using packed_bytes_trait_max =
+    std::conditional_t<packed_bytes_trait<32>::available, packed_bytes_trait<32>,
+                       std::conditional_t<packed_bytes_trait<16>::available, packed_bytes_trait<16>,
+                                          std::conditional_t<packed_bytes_trait<8>::available, packed_bytes_trait<8>,
+                                                             packed_bytes_trait<4>>>>;
--- include/packed_bytes_x86.hpp
+++ include/packed_bytes_x86.hpp
@@ -2,21 +2,24 @@
 #include "packed_bytes.hpp"
 
 #include <emmintrin.h>
-#if defined(__SSE4_1__) || defined(__AVX2__) || defined(_MSC_VER) 
+#if defined(__SSE4_1__) || defined(__AVX2__) || defined(_MSC_VER)
 // MSVC enables all SSE4.1 intrinsics by default
 #include <smmintrin.h>
 #endif
 
-struct packed_bytes_trait_sse {
+struct packed_bytes_trait_sse
+{
     static constexpr bool available = true;
     static constexpr auto step = 16;
     using value_type = __m128i;
 
-    __packed_bytes_strong_inline static value_type load_unaligned(const void *ptr) {
+    __packed_bytes_strong_inline static value_type load_unaligned(const void* ptr)
+    {
         return _mm_loadu_si128(reinterpret_cast<const __m128i*>(ptr));
     }
 
-    __packed_bytes_strong_inline static value_type less(value_type x, uint8_t n) {
+    __packed_bytes_strong_inline static value_type less(value_type x, uint8_t n)
+    {
         auto bcast = _mm_set1_epi8(static_cast<char>(n));
         auto all1 = _mm_set1_epi8(-1);
         auto max_with_n = _mm_max_epu8(x, bcast);
@@ -25,19 +28,17 @@ struct packed_bytes_trait_sse {
         return is_less;
     }
 
-    __packed_bytes_strong_inline static value_type equal(value_type x, uint8_t n) {
+    __packed_bytes_strong_inline static value_type equal(value_type x, uint8_t n)
+    {
         return _mm_cmpeq_epi8(x, _mm_set1_epi8(static_cast<char>(n)));
     }
 
-    __packed_bytes_strong_inline static value_type equal(value_type x, value_type y) {
-        return _mm_cmpeq_epi8(x, y);
-    }
+    __packed_bytes_strong_inline static value_type equal(value_type x, value_type y) { return _mm_cmpeq_epi8(x, y); }
 
-    __packed_bytes_strong_inline static value_type bitwise_or(value_type a, value_type b) {
-        return _mm_or_si128(a, b);
-    }
+    __packed_bytes_strong_inline static value_type bitwise_or(value_type a, value_type b) { return _mm_or_si128(a, b); }
 
-    __packed_bytes_strong_inline static bool is_all_zero(value_type x) {
+    __packed_bytes_strong_inline static bool is_all_zero(value_type x)
+    {
 #if defined(__SSE4_1__) || defined(__AVX2__) || defined(_MSC_VER)
         // SSE4.1 path
         return !!_mm_testz_si128(x, x);
@@ -49,7 +50,8 @@ struct packed_bytes_trait_sse {
 #endif
     }
 
-    __packed_bytes_strong_inline static size_t first_nonzero_byte(value_type x) {
+    __packed_bytes_strong_inline static size_t first_nonzero_byte(value_type x)
+    {
         auto cmp = _mm_cmpeq_epi8(x, _mm_set1_epi8(0));
         auto mask = (uint16_t)_mm_movemask_epi8(cmp);
         return json::__bitops::countr_one((uint32_t)mask);
@@ -57,10 +59,10 @@ struct packed_bytes_trait_sse {
 };
 
 template <>
-struct packed_bytes<16> {
+struct packed_bytes<16>
+{
     using traits = packed_bytes_trait_sse;
 };
-
 
 #ifdef __AVX2__
 #include <immintrin.h>
@@ -91,20 +93,14 @@ struct packed_bytes_trait_avx2
         return _mm256_cmpeq_epi8(x, _mm256_set1_epi8(static_cast<char>(n)));
     }
 
-    __packed_bytes_strong_inline static value_type equal(value_type x, value_type y)
-    {
-        return _mm256_cmpeq_epi8(x, y);
-    }
+    __packed_bytes_strong_inline static value_type equal(value_type x, value_type y) { return _mm256_cmpeq_epi8(x, y); }
 
     __packed_bytes_strong_inline static value_type bitwise_or(value_type a, value_type b)
     {
         return _mm256_or_si256(a, b);
     }
 
-    __packed_bytes_strong_inline static bool is_all_zero(value_type x)
-    {
-        return (bool)_mm256_testz_si256(x, x);
-    }
+    __packed_bytes_strong_inline static bool is_all_zero(value_type x) { return (bool)_mm256_testz_si256(x, x); }
 
     __packed_bytes_strong_inline static size_t first_nonzero_byte(value_type x)
     {
@@ -116,7 +112,8 @@ struct packed_bytes_trait_avx2
 };
 
 template <>
-struct packed_bytes<32> {
+struct packed_bytes<32>
+{
     using traits = packed_bytes_trait_avx2;
 };
 
