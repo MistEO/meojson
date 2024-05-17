baab5dc673336d264012b0745ee17d04fce81afe
nekosu <liao.hy@outlook.com>
2024-05-17 20:04:43 +0800

    feat: unicode pair

--- test/unicode_test.cpp
+++ test/unicode_test.cpp
@@ -5,7 +5,8 @@
 
 bool unicode_test()
 {
-    std::string test = R"({ "test": "abc\u9a8fdef" })";
+    std::string test = R"({ "test": "abc\u9a8fdef\ud83d\udca9ghi" })";
+    std::string target = "abc\u9a8fdef\U0001f4a9ghi";
     auto obj = json::parse(test);
     if (!obj.has_value()) {
         std::cout << "parse failed" << std::endl;
@@ -16,10 +17,10 @@ bool unicode_test()
                   << static_cast<unsigned>(static_cast<unsigned char>(ch)) << ' ';
     }
     std::cout << std::endl;
-    for (auto ch : "abc\u9a8fdef") {
+    for (auto ch : target) {
         std::cout << std::hex << std::setw(2)
                   << static_cast<unsigned>(static_cast<unsigned char>(ch)) << ' ';
     }
     std::cout << std::endl;
-    return obj.value().at("test").as_string() == "abc\u9a8fdef";
+    return obj.value().at("test").as_string() == target;
 }
