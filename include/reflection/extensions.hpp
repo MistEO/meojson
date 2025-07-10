abe08bec6bee23779ff96524acb4367804e1623c
nekosu <liao.hy@outlook.com>
2025-07-10 16:44:20 +0800

    fix: add string_t

--- include/reflection/extensions.hpp
+++ include/reflection/extensions.hpp
@@ -17,26 +17,40 @@ template <>
 class jsonization<std::nullptr_t>
 {
 public:
-    json::value to_json(const std::nullptr_t&) const { return json::value {}; }
+    template <typename string_t>
+    json::basic_value<string_t> to_json(const std::nullptr_t&) const
+    {
+        return json::basic_value<string_t> {};
+    }
 
-    bool check_json(const json::value& j) const { return j.is_null(); }
+    template <typename string_t>
+    bool check_json(const json::basic_value<string_t>& j) const
+    {
+        return j.is_null();
+    }
 
-    bool from_json(const json::value& j, std::nullptr_t&) { return check_json(j); }
+    template <typename string_t>
+    bool from_json(const json::basic_value<string_t>& j, std::nullptr_t&)
+    {
+        return check_json(j);
+    }
 };
 
 template <typename elem1_t, typename elem2_t>
 class jsonization<std::pair<elem1_t, elem2_t>>
 {
 public:
-    json::value to_json(const std::pair<elem1_t, elem2_t>& value) const
+    template <typename string_t>
+    json::basic_value<string_t> to_json(const std::pair<elem1_t, elem2_t>& value) const
     {
-        return json::array {
-            json::value { value.first },
-            json::value { value.second },
+        return json::basic_array<string_t> {
+            json::basic_value<string_t> { value.first },
+            json::basic_value<string_t> { value.second },
         };
     }
 
-    bool check_json(const json::value& j) const
+    template <typename string_t>
+    bool check_json(const json::basic_value<string_t>& j) const
     {
         if (!j.is_array()) {
             return false;
@@ -45,10 +59,11 @@ public:
         if (arr.size() != 2) {
             return false;
         }
-        return arr[0].is<elem1_t>() && arr[1].is<elem2_t>();
+        return arr[0].template is<elem1_t>() && arr[1].template is<elem2_t>();
     }
 
-    bool from_json(const json::value& j, std::pair<elem1_t, elem2_t>& value) const
+    template <typename string_t>
+    bool from_json(const json::basic_value<string_t>& j, std::pair<elem1_t, elem2_t>& value) const
     {
         if (!j.is_array()) {
             return false;
@@ -57,11 +72,11 @@ public:
         if (arr.size() != 2) {
             return false;
         }
-        if (!(arr[0].is<elem1_t>() && arr[1].is<elem2_t>())) {
+        if (!(arr[0].template is<elem1_t>() && arr[1].template is<elem2_t>())) {
             return false;
         }
-        value.first = arr[0].as<elem1_t>();
-        value.second = arr[1].as<elem2_t>();
+        value.first = arr[0].template as<elem1_t>();
+        value.second = arr[1].template as<elem2_t>();
         return true;
     }
 };
@@ -73,20 +88,24 @@ public:
     using tuple_t = std::tuple<args_t...>;
     constexpr static size_t tuple_size = std::tuple_size_v<tuple_t>;
 
-    json::value to_json(const tuple_t& value) const
+    template <typename string_t>
+    json::basic_value<string_t> to_json(const tuple_t& value) const
     {
-        json::array result;
+        json::basic_array<string_t> result;
         to_json_impl(result, value, std::make_index_sequence<tuple_size>());
         return result;
     }
 
-    template <std::size_t... Is>
-    void to_json_impl(json::array& arr, const tuple_t& t, std::index_sequence<Is...>) const
+    template <typename string_t, std::size_t... Is>
+    void
+        to_json_impl(json::basic_array<string_t>& arr, const tuple_t& t, std::index_sequence<Is...>)
+            const
     {
         (arr.emplace_back(std::get<Is>(t)), ...);
     }
 
-    bool check_json(const json::value& j) const
+    template <typename string_t>
+    bool check_json(const json::basic_value<string_t>& j) const
     {
         if (!j.is_array()) {
             return false;
@@ -99,13 +118,14 @@ public:
         return check_json_impl(arr, std::make_index_sequence<tuple_size>());
     }
 
-    template <std::size_t... Is>
-    bool check_json_impl(const json::array& arr, std::index_sequence<Is...>) const
+    template <typename string_t, std::size_t... Is>
+    bool check_json_impl(const json::basic_array<string_t>& arr, std::index_sequence<Is...>) const
     {
-        return (arr[Is].is<std::tuple_element_t<Is, tuple_t>>() && ...);
+        return (arr[Is].template is<std::tuple_element_t<Is, tuple_t>>() && ...);
     }
 
-    bool from_json(const json::value& j, tuple_t& value) const
+    template <typename string_t>
+    bool from_json(const json::basic_value<string_t>& j, tuple_t& value) const
     {
         if (!j.is_array()) {
             return false;
@@ -123,10 +143,13 @@ public:
         return true;
     }
 
-    template <std::size_t... Is>
-    void from_json_impl(const json::array& arr, tuple_t& t, std::index_sequence<Is...>) const
+    template <typename string_t, std::size_t... Is>
+    void from_json_impl(
+        const json::basic_array<string_t>& arr,
+        tuple_t& t,
+        std::index_sequence<Is...>) const
     {
-        ((std::get<Is>(t) = arr[Is].as<std::tuple_element_t<Is, tuple_t>>()), ...);
+        ((std::get<Is>(t) = arr[Is].template as<std::tuple_element_t<Is, tuple_t>>()), ...);
     }
 };
 
@@ -137,31 +160,37 @@ public:
     using variant_t = std::variant<args_t...>;
     constexpr static size_t variant_size = std::variant_size_v<variant_t>;
 
-    json::value to_json(const variant_t& value) const
+    template <typename string_t>
+    json::basic_value<string_t> to_json(const variant_t& value) const
     {
-        json::value result;
+        json::basic_value<string_t> result;
         to_json_impl(result, value, std::make_index_sequence<variant_size>());
         return result;
     }
 
-    template <std::size_t... Is>
-    void to_json_impl(json::value& val, const variant_t& t, std::index_sequence<Is...>) const
+    template <typename string_t, std::size_t... Is>
+    void to_json_impl(
+        json::basic_value<string_t>& val,
+        const variant_t& t,
+        std::index_sequence<Is...>) const
     {
         ((t.index() == Is ? (val = std::get<Is>(t), true) : false) || ...);
     }
 
-    bool check_json(const json::value& j) const
+    template <typename string_t>
+    bool check_json(const json::basic_value<string_t>& j) const
     {
         return check_json_impl(j, std::make_index_sequence<variant_size>());
     }
 
-    template <std::size_t... Is>
-    bool check_json_impl(const json::value& val, std::index_sequence<Is...>) const
+    template <typename string_t, std::size_t... Is>
+    bool check_json_impl(const json::basic_value<string_t>& val, std::index_sequence<Is...>) const
     {
-        return (val.is<std::variant_alternative_t<Is, variant_t>>() || ...);
+        return (val.template is<std::variant_alternative_t<Is, variant_t>>() || ...);
     }
 
-    bool from_json(const json::value& j, variant_t& value) const
+    template <typename string_t>
+    bool from_json(const json::basic_value<string_t>& j, variant_t& value) const
     {
         if (!check_json_impl(j, std::make_index_sequence<variant_size>())) {
             return false;
@@ -171,12 +200,15 @@ public:
         return true;
     }
 
-    template <std::size_t... Is>
-    void from_json_impl(const json::value& j, variant_t& t, std::index_sequence<Is...>) const
+    template <typename string_t, std::size_t... Is>
+    void from_json_impl(
+        const json::basic_value<string_t>& j,
+        variant_t& t,
+        std::index_sequence<Is...>) const
     {
         std::ignore =
-            ((j.is<std::variant_alternative_t<Is, variant_t>>()
-                  ? (t = j.as<std::variant_alternative_t<Is, variant_t>>(), true)
+            ((j.template is<std::variant_alternative_t<Is, variant_t>>()
+                  ? (t = j.template as<std::variant_alternative_t<Is, variant_t>>(), true)
                   : false)
              || ...);
     }
