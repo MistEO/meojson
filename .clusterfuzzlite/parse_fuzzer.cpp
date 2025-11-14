1be37297eaf11984c3b41178a56836aacc971615
MistEO <mistereo@hotmail.com>
2025-11-14 20:28:31 +0800

    ci: fix fuzzer

--- .clusterfuzzlite/parse_fuzzer.cpp
+++ .clusterfuzzlite/parse_fuzzer.cpp
@@ -1,12 +1,11 @@
 #include <string_view>
 
 #include "json.hpp"
-#include "json5.hpp"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
     std::string_view content(reinterpret_cast<const char*>(data), size);
     json::parse(content);
-    json::parse5(content);
+    json::parsec(content);
     return 0;
 }
